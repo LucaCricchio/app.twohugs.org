46fe6e1987b3bbb6db22e50917b2d3ee5e5fac0c
Francesco <francesco.pepe91@gmail.com>
2017-07-13 18:28:48 +0200

    Added note

--- app/Models/SearchList.php
+++ app/Models/SearchList.php
@@ -54,7 +54,7 @@ class SearchList extends Model
          */
         $search      = Search::whereId($this->search_id)->first();
         $lastTimeout = Carbon::now()->subMinutes(5)->toDateTimeString();
-	    //$lastUpdate  = Carbon::now()->subMinutes(10)->toDateTimeString();
+	    //$lastUpdate  = Carbon::now()->subMinutes(10)->toDateTimeString(); // TODO: Da fixare in produzione
 	    $lastUpdate  = Carbon::now()->subHours(240)->toDateTimeString();
 
 	    $usersWhoRefused     = $search->usersWhoRefused();
